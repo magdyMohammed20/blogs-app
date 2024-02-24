a5cd1f6f8752b7e206477696ea094627a67ec598
magdyMohammed20 <magdymohammed37@yahoo.com>
2024-02-24 21:11:37 +0200

    Fix Problem Of Refetch Data When Search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -13,21 +13,24 @@ import {
 
 const SearchComponent = () => {
   const [searchQuery, setSearchQuery] = useState(null); // State For Store The Current Search Query
+  const [isSearchClicked, setIsSearchClicked] = useState(false);
   const dispatch = useDispatch();
 
   // Function For Set The Search Query In State
   const handleSearchChange = (val) => {
     setSearchQuery(val);
 
-    // If Search Query Is Empty While Typing Fetch The First Chunk Of Data
-    if (!val) {
+    // If Search Query Is Empty While Typing And Search Button Clicked Fetch The First Chunk Of Data
+    if (!val && isSearchClicked) {
       dispatch(fetchBlogs());
+      setIsSearchClicked(false);
     }
   };
 
   // Function For Dispatch The Search Action
   const submitSearch = () => {
     dispatch(searchForArticle(searchQuery));
+    setIsSearchClicked(true); // For Mark Search Button Clicked To Control Refetch All News If Input Changed To Empty
   };
 
   return (
@@ -39,7 +42,14 @@ const SearchComponent = () => {
         <InputField onChange={(e) => handleSearchChange(e.target.value)} />
       </InputWrapper>
 
-      <Button onClick={submitSearch}>
+      <Button
+        onClick={submitSearch}
+        disabled={!searchQuery}
+        className={`border ${
+          !searchQuery
+            ? "bg-gray-500"
+            : "bg-blue-600  border-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        }`}>
         <Search size={18} className="text-white font-bold" />
       </Button>
     </SearchParent>
--- src/components/Search/elements.js
+++ src/components/Search/elements.js
@@ -21,7 +21,7 @@ const InputField = styled.input.attrs({
 
 
 const Button = styled.button.attrs({
-    className: "p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800",
+    className: "p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg  focus:ring-4 focus:outline-none ",
 })``;
 
 
